ff48c565741131611a79da7f4d714c06ba752b1a
Denis Denisov <denis.denisov8701@gmail.com>
2024-03-15 11:36:55 +0100

    model Location

---
+++ CheckYourVPN/Models/Location.swift
@@ -0,0 +1,24 @@
+//
+//  Location.swift
+//  CheckYourVPN
+//
+//  Created by Denis Denisov on 15/3/24.
+//
+
+import Foundation
+
+struct Location {
+    let continent: String
+    let country: String
+    let regionName: String
+    let city: String
+    let ip: String
+    
+    var description: String {
+        """
+        Ты находишься в \(country), \(continent)
+        А точнее в \(city), \(regionName)
+        Твой ip: \(ip)
+        """
+    }
+}
